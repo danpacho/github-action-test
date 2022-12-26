f7716935ee8715cfa55cac44d72171016a5af409
Danpa_cho <danpa725@cau.ac.kr>
2022-12-26 16:55:07 +0900

    feat add log package versionfeature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-const firstVersion = 1;
+// @ts-check
+import chalk from "chalk";
+import { readFile } from "fs/promises";
 
-export { firstVersion };
+/**
+ * @param {string} filePath
+ * @returns {Promise<{version: string}>} JSON string
+ */
+const readJSON = async (filePath) =>
+    // @ts-ignore
+    JSON.parse(await readFile(new URL(filePath, import.meta.url)));
+
+/**@typedef {{major: string, minor: string, patch: string}} Version */
+/**
+ * @param {string} versionString
+ * @returns {Version} versionOption
+ */
+const getVersionNumber = (versionString) => {
+    const [major, minor, patch] = String(versionString).split(".");
+    return {
+        major,
+        minor,
+        patch,
+    };
+};
+
+/**
+ * @param {Version} versionOption
+ * @returns version string
+ */
+const getVersionHeader = ({ major, minor, patch }) =>
+    `   version: ${major}.${minor}.${patch}   `;
+
+/**
+ * @param {string} versionString
+ */
+const logVersion = (versionString) => {
+    console.log(chalk.bgMagentaBright.black.bold(versionString));
+};
+
+(async () => {
+    const packageJSON = await readJSON("../package.json");
+    const versionOption = getVersionNumber(packageJSON.version);
+    const versionHeader = getVersionHeader(versionOption);
+    logVersion(versionHeader);
+})();
