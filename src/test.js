bdec352f5beb6246644007abc0ff2bde0b11c3df
Danpa_cho <danpa725@cau.ac.kr>
2022-12-27 14:45:22 +0900

    feat add complex test runner

--- src/test.js
+++ src/test.js
@@ -1,5 +1,26 @@
 import chalk from "chalk";
 
-(() => {
-    console.log(chalk.bgRed.white.bold("   test: succeeded   "));
-})();
+/**
+ * @param {string} string
+ * @returns capitalized case string
+ */
+const toCapitalCase = (string) =>
+    `${string[0].toUpperCase()}${string.slice(1, string.length)}`;
+
+/**
+ * @param {string[]} tests
+ * @param {"red" | "yellow" | "green"} color
+ */
+const testRunner = (tests, color = "red") => {
+    const testChalk = chalk.white.bold;
+    const testLogger = (message, chalkOption) =>
+        console.log(testChalk[chalkOption](`   ${message}   `));
+
+    tests.forEach((test, order) => {
+        testLogger(`test${order + 1}: ${test}`, `bg${toCapitalCase(color)}`);
+    });
+};
+
+const tests = ["good", "too good", "hello test"];
+
+testRunner(tests, "yellow");
